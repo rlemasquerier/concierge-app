4d0136c6f5191f4c524c2c28a82ddbf0c852d566
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-01-16 23:08:10 +0100

    added colors and font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import injectSheet from "react-jss";
+import Switch from "react-switch";
+import theme from "./theme";
+import "./index.css";
 
 class App extends Component {
   render() {
-    return <div>Content</div>;
+    const { classes } = this.props;
+    return (
+      <div className={classes.container}>
+        <div className={classes.header}>Concierge App</div>
+        <div className={classes.main}>
+          <div>Content</div>
+          <Switch />
+        </div>
+      </div>
+    );
   }
 }
 
-export default App;
+const styles = {
+  container: {
+    fontFamily: "Fredoka One",
+    position: "fixed",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
+    backgroundColor: theme.colors.lightGreen
+  },
+  header: {
+    color: theme.colors.red,
+    flex: 1
+  },
+  main: {
+    flex: 4
+  }
+};
+
+export default injectSheet(styles)(App);
