5506ccbd78da31528d0f5986fd30c805d0e69120
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-01-21 00:06:48 +0100

    header styling

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -9,30 +9,43 @@ class Header extends Component {
     return (
       <div className={classes.header}>
         <img className={classes.logo} src={logo} alt="concierge" />
-        <div className={classes.headerText}>Welcome to Concierge App !</div>
+        <div className={classes.headerContainer}>
+          Welcome to Concierge App !
+        </div>
       </div>
     );
   }
 }
 
 const styles = {
-  headerText: {
-    fontSize: 30
+  headerContainer: {
+    flex: 1,
+    fontSize: 30,
+    textAlign: "center",
+    "@media (max-width: 500px)": {
+      fontSize: 20
+    }
   },
   logo: {
+    flex: 1,
     marginTop: 20,
     marginLeft: 20,
     height: "auto",
     width: "auto",
     maxWidth: 150,
-    maxHeight: 150
+    maxHeight: 150,
+    "@media (max-width: 500px)": {
+      maxWidth: 100,
+      maxheight: 100
+    }
   },
   header: {
     color: theme.colors.red,
     flex: 1,
     display: "flex",
     flexDirection: "row",
-    justifyContent: "flex-start"
+    justifyContent: "flex-start",
+    alignItems: "center"
   }
 };
 
