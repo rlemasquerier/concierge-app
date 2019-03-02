7e08da7d0095b57afdca820fc8891a1eed2873b3
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-03-02 14:45:53 +0100

    Installed flow typed

--- src/redux/actions/simpleAction.js
+++ src/redux/actions/simpleAction.js
@@ -1,6 +1,6 @@
 // @flow
 
-export const simpleAction = () => (dispatch: Function): Function => {
+export const simpleAction = () => (dispatch: Function): any => {
   dispatch({
     type: 'SIMPLE_ACTION',
     payload: 'result_of_simple_action',
